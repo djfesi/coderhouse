10fdf3328690f50b09a8924dc2ccdd3ed23ae6df
Federico <you@example.com>
2023-09-19 21:15:25 -0300

    Entrega Final

--- js/main.js
+++ js/main.js
@@ -5,65 +5,6 @@ const PIZZA_SIZES = {
   3: { size: 4, price: 1200 },
 };
 
-const PRODUCTS = [
-  {
-    id: 0,
-    name: "Pizza Cantimpalo",
-    description:
-      "Nuestra deliciosa pizza Cantimpalo está hecha con una base de salsa de tomate casera, queso mozzarella derretido y abundantes rodajas de chorizo Cantimpalo, ¡una explosión de sabor en cada bocado!",
-    type: "Pizza",
-    img: "https://th.bing.com/th/id/R.fb924adfcac2d9873093f80618bb137b?rik=MqV9PfXgBoPm5w&riu=http%3a%2f%2fpapaallenspizza.com%2fdowntown%2fimages%2fpepperoni.png&ehk=4%2bn5e2Z7XXIlHYGlmA42b15HqPlvRyrfuPfwzsLVY9s%3d&risl=&pid=ImgRaw&r=0",
-  },
-  {
-    id: 1,
-    name: "Pizza Margherita",
-    description:
-      "Nuestra clásica Pizza Margherita es una obra maestra simple pero deliciosa. Con tomates frescos, mozzarella de alta calidad y hojas de albahaca fragantes, te transportará a la Italia auténtica en cada bocado.",
-    type: "Pizza",
-    img: "https://www.hearthandfirepizza.com/cdn/shop/products/product-beauty-the-margherita.png?v=1656423819",
-  },
-  {
-    id: 2,
-    name: "Pizza BBQ",
-    description:
-      "Si eres amante de los sabores ahumados y el toque agridulce, nuestra Pizza BBQ es perfecta para ti. Con salsa barbacoa, pollo a la parrilla, cebolla roja y queso cheddar, te llevará a una experiencia única.",
-    type: "Pizza",
-    img: "https://images.newyorkpizza.nl/Products/Original/BBQ_Double_Bacon-8181.png",
-  },
-  {
-    id: 3,
-    name: "Pizza Vegetariana",
-    description:
-      "Nuestra Pizza Vegetariana es un festín de sabores frescos. Con una mezcla de pimientos, champiñones, aceitunas, cebolla y espinacas, es una opción saludable y deliciosa para los amantes de las verduras.",
-    type: "Pizza",
-    img: "https://livornos.com/wp-content/uploads/2020/08/h2_pizza-1.png",
-  },
-  {
-    id: 4,
-    name: "Pizza Hawaiana",
-    description:
-      "La Pizza Hawaiana es la combinación perfecta de dulce y salado. Con trozos jugosos de piña, jamón, salsa de tomate y queso fundido, te transportará a una isla tropical en cada bocado.",
-    type: "Pizza",
-    img: "https://www.pizzapatron.com/wp-content/themes/patron/dist/img/hawaianawebP.webp",
-  },
-  {
-    id: 5,
-    name: "Pizza Pepperoni",
-    description:
-      "Nuestra Pizza Pepperoni es un clásico atemporal. Con generosas capas de pepperoni y queso derretido, es la elección perfecta para los amantes de los sabores intensos y picantes.",
-    type: "Pizza",
-    img: "https://assets.caseys.com/m/429f598e526c218c/400x400-8104_base.PNG",
-  },
-  {
-    id: 6,
-    name: "Pizza de Pollo BBQ",
-    description:
-      "¿Quieres un toque de barbacoa en tu pizza? Prueba nuestra Pizza de Pollo BBQ, con pollo a la parrilla, cebolla morada, maíz dulce y salsa de barbacoa, es una fiesta de sabores en cada rebanada.",
-    type: "Pizza",
-    img: "https://eldinamico.com/wp-content/uploads/2023/03/BBQ.png",
-  },
-];
-
 // Elementos capturados
 const productsHTML = document.getElementById("cardProducts");
 const inputSearch = document.querySelector("#search");
@@ -72,6 +13,7 @@ const processOrderButton = document.querySelector(".btn-process");
 const purchaseDetails = document.getElementById("purchaseDetails");
 
 //Variables ============================================================================================
+let PRODUCTS = [];
 let loadingProducts = true;
 let totalPrice = 0;
 let totalQuantity = 0;
@@ -79,10 +21,10 @@ let order = [];
 let cart = [];
 
 //Funciones ============================================================================================
-
 //Creacion de Cards de Productos
 function createGrid(PRODUCTS) {
   for (const product of PRODUCTS) {
+    const { id, img, name, description } = product;
     let card = document.createElement("div");
     card.classList.add("col-sm-12");
     card.classList.add("col-md-6");
@@ -90,23 +32,23 @@ function createGrid(PRODUCTS) {
     card.classList.add("p-4");
     card.innerHTML = `<section class="card">
     <div class="product-image">
-      <img src=${product.img} draggable="false" />         
+      <img src=${img} draggable="false" />         
     </div>
     <div class="product-info">
-      <h2>🍕 ${product.name}</h2>
-      <p>${product.description}</p>
-      <div class="price" id="price-${product.id}">$${PIZZA_SIZES[1].price}</div>
+      <h2>🍕 ${name}</h2>
+      <p>${description}</p>
+      <div class="price" id="price-${id}">$${PIZZA_SIZES[1].price}</div>
       <label for="portion-select">Porción:</label>
-      <select id="portion-select-${product.id}">
+      <select id="portion-select-${id}">
         <option value="1">12 porciones</option>
         <option value="2">8 porciones</option>
         <option value="3">4 porciones</option>
       </select>
     </div>
     <div class="btn">
-      <button class="buy-btn" id="${product.id}">Agregar al carrito 👨‍🍳</button>
-      <button class="fav" id="fav-${product.id}">
-        <svg class="svg" id="i-star-${product.id}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <button class="buy-btn" id="${id}">Agregar al carrito 👨‍🍳</button>
+      <button class="fav" id="fav-${id}">
+        <svg class="svg" id="i-star-${id}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
           <path d="M16 2 L20 12 30 12 22 19 25 30 16 23 7 30 10 19 2 12 12 12 Z" />
         </svg>
       </button>
@@ -114,17 +56,15 @@ function createGrid(PRODUCTS) {
     </section>`;
 
     productsHTML.appendChild(card);
-    const favButton = document.getElementById(`fav-${product.id}`);
+    const favButton = document.getElementById(`fav-${id}`);
 
-    favButton.addEventListener("click", () => toggleFavorite(product.id));
+    favButton.addEventListener("click", () => toggleFavorite(id));
 
-    const portionSelect = document.getElementById(
-      `portion-select-${product.id}`
-    );
+    const portionSelect = document.getElementById(`portion-select-${id}`);
 
     portionSelect.addEventListener("change", () => {
       const selectedSize = portionSelect.value;
-      const priceElement = document.getElementById(`price-${product.id}`);
+      const priceElement = document.getElementById(`price-${id}`);
       priceElement.textContent = `$${PIZZA_SIZES[selectedSize].price}`;
     });
   }
@@ -167,11 +107,9 @@ function applyFavoriteStateOnLoad() {
     const productId = product.id;
     const svgIcon = document.getElementById(`i-star-${productId}`);
     const isFavorite = localStorage.getItem(`favorite-${productId}`);
-    if (isFavorite === "true") {
-      svgIcon.style.fill = "#ffcc00";
-    } else {
-      svgIcon.style.fill = "#fff";
-    }
+    isFavorite === "true"
+      ? (svgIcon.style.fill = "#ffcc00")
+      : (svgIcon.style.fill = "#fff");
   });
 }
 
@@ -245,12 +183,18 @@ function addToCart(button) {
     cart[existingProductIndex].amount += 1;
   } else {
     selectedProduct.amount = 1;
-    delete selectedProduct.img;
-    delete selectedProduct.description;
     cart.push(selectedProduct);
   }
 
   sessionStorage.setItem("cartItems", JSON.stringify(cart));
+
+  Toastify({
+    text: `${selectedProduct.name} agregada`,
+    duration: 1200,
+    style: {
+      background: "linear-gradient(to right, #b99811, #ffcc00)",
+    },
+  }).showToast();
 
   if (purchaseDetails.textContent) {
     purchaseDetails.classList.remove("bg-detail");
@@ -280,25 +224,37 @@ function processOrder() {
   clearCart();
 }
 
-//Se llama a las funciones
-createGrid(PRODUCTS);
-if (sessionStorage.getItem("cartItems")) {
-  cart = JSON.parse(sessionStorage.getItem("cartItems"));
-}
-updateCounter();
-renderCartTable();
-applyFavoriteStateOnLoad();
+//Fetch consumir productos
+fetch("./data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    PRODUCTS = data;
+    createGrid(PRODUCTS);
+    if (sessionStorage.getItem("cartItems")) {
+      cart = JSON.parse(sessionStorage.getItem("cartItems"));
+    }
+    updateCounter();
+    renderCartTable();
+    applyFavoriteStateOnLoad();
+    let addButtons = document.querySelectorAll(".buy-btn");
+    addButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        addToCart(button);
+      });
+    });
+  })
+  .catch((err) => {
+    Toastify({
+      text: "No se pueden cargar los productos.",
+      duration: 2000,
+      style: {
+        background: "linear-gradient(to right, #a62121, #d71a1a)",
+      },
+    }).showToast();
+  });
 //Fin
 
 //Eventos ============================================================================================
-// Agregar event listener a los botones de compra
-let addButtons = document.querySelectorAll(".buy-btn");
-addButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    addToCart(button);
-  });
-});
-
 // Filtro de busqueda
 inputSearch.addEventListener("keyup", () => {
   const searchText = inputSearch.value.toLowerCase();
@@ -318,7 +274,31 @@ inputSearch.addEventListener("keyup", () => {
 });
 
 //Limpiar carrito
-clearCartButton.addEventListener("click", clearCart);
+clearCartButton.addEventListener("click", function () {
+  if (!sessionStorage.getItem("cartItems")) {
+    Toastify({
+      text: "No tienes productos cargados.",
+      duration: 1200,
+      style: {
+        background: "linear-gradient(to right, #a62121, #d71a1a)",
+      },
+    }).showToast();
+  } else {
+    Swal.fire({
+      title: "Vaciar Carrito",
+      text: "Esta seguro que desea vaciar el carrito de compras?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#ffcc00",
+      cancelButtonColor: "#a62121",
+      confirmButtonText: "Vaciar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clearCart();
+      }
+    });
+  }
+});
 
 //Finalizar compra
 processOrderButton.addEventListener("click", processOrder);
